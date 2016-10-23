179dd902ebc800b90a57fa7b2391ba47769bc9c4
robwebset <robwebset@users.noreply.github.com>
2016-10-23 17:04:13 +0100

    Update Links

--- README.md
+++ README.md
@@ -2,16 +2,12 @@
 
 VideoScreensaver is a screensaver that will play a video file when the screensaver starts. It will support playing either a user specified video file, or one of the following pre-defined video files detailed on the Wiki.
 
-[Add-on:VideoScreensaver](http://kodi.wiki/view/Add-on:VideoScreensaver)
+[Add-on:VideoScreensaver](https://github.com/robwebset/screensaver.video/wiki)
 
-You can install this addon from: [Repository:robwebset](https://github.com/robwebset/repository.robwebset/blob/master/repos/repository.robwebset/repository.robwebset-1.0.0.zip) ([wiki](http://kodi.wiki/view/Repository:robwebset))
-
-_A Special Thankyou to ziggy73701 for helping supply some storage space to improve download of the Videos!_
+You can install this addon from: [Repository:robwebset](https://github.com/robwebset/repository.robwebset/blob/master/repos/repository.robwebset/repository.robwebset-1.0.0.zip) ([wiki](https://github.com/robwebset/repository.robwebset/wiki))
 
 __Donations__
 
 If you have found VideoScreensaver useful and would like to help support future development and bug fixes, then please feel free to make a small donation.
 
 [![Donation](https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=A2G8ES4GVMF6J)
-
-License: See [LICENSE.txt](LICENSE.txt)
--- service.py
+++ service.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import xbmc
 import xbmcaddon
+
+if sys.version_info >= (2, 7):
+    import json
+else:
+    import simplejson as json
 
 # Import the common settings
 from resources.lib.settings import log
@@ -17,19 +23,31 @@ CWD = ADDON.getAddonInfo('path').decode("utf-8")
 if __name__ == '__main__':
     log("VideoScreensaverService: Startup checks")
 
-    # Check if the settings mean we want to reset the volume on startup
-    startupVol = Settings.getStartupVolume()
+    json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.robwebset", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
+    json_response = json.loads(json_query)
 
-    if startupVol < 0:
-        log("VideoScreensaverService: No Volume Change Required")
-    else:
-        log("VideoScreensaverService: Setting volume to %s" % startupVol)
-        xbmc.executebuiltin('SetVolume(%d)' % startupVol, True)
+    displayNotice = True
+    if ("result" in json_response) and ('addon' in json_response['result']):
+        addonItem = json_response['result']['addon']
+        if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.robwebset') and (addonItem['author'] == 'robwebset'):
+            displayNotice = False
 
-    # Make sure that the settings have been updated correctly
-    Settings.cleanAddonSettings()
+            # Check if the settings mean we want to reset the volume on startup
+            startupVol = Settings.getStartupVolume()
 
-    # Check if we should start the screensaver video on startup
-    if Settings.isLaunchOnStartup():
-        log("VideoScreensaverService: Launching screensaver video on startup")
-        xbmc.executebuiltin('RunScript(%s)' % (os.path.join(CWD, "screensaver.py")))
+            if startupVol < 0:
+                log("VideoScreensaverService: No Volume Change Required")
+            else:
+                log("VideoScreensaverService: Setting volume to %s" % startupVol)
+                xbmc.executebuiltin('SetVolume(%d)' % startupVol, True)
+
+            # Make sure that the settings have been updated correctly
+            Settings.cleanAddonSettings()
+
+            # Check if we should start the screensaver video on startup
+            if Settings.isLaunchOnStartup():
+                log("VideoScreensaverService: Launching screensaver video on startup")
+                xbmc.executebuiltin('RunScript(%s)' % (os.path.join(CWD, "screensaver.py")))
+
+    if displayNotice:
+        xbmc.executebuiltin('Notification("robwebset Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
