85c955e5a356aef478526eec077ae8eb1d0b97c2
robwebset <robwebset@users.noreply.github.com>
2016-12-17 18:57:10 +0000

    Update for support of URepo

--- service.py
+++ service.py
@@ -32,22 +32,31 @@ if __name__ == '__main__':
         if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.robwebset') and (addonItem['author'] == 'robwebset'):
             displayNotice = False
 
-            # Check if the settings mean we want to reset the volume on startup
-            startupVol = Settings.getStartupVolume()
+    if displayNotice:
+        json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.urepo", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
+        json_response = json.loads(json_query)
 
-            if startupVol < 0:
-                log("VideoScreensaverService: No Volume Change Required")
-            else:
-                log("VideoScreensaverService: Setting volume to %s" % startupVol)
-                xbmc.executebuiltin('SetVolume(%d)' % startupVol, True)
-
-            # Make sure that the settings have been updated correctly
-            Settings.cleanAddonSettings()
-
-            # Check if we should start the screensaver video on startup
-            if Settings.isLaunchOnStartup():
-                log("VideoScreensaverService: Launching screensaver video on startup")
-                xbmc.executebuiltin('RunScript(%s)' % (os.path.join(CWD, "screensaver.py")))
+        if ("result" in json_response) and ('addon' in json_response['result']):
+            addonItem = json_response['result']['addon']
+            if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.urepo'):
+                displayNotice = False
 
     if displayNotice:
-        xbmc.executebuiltin('Notification("robwebset Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
+        xbmc.executebuiltin('Notification("robwebset or URepo Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
+    else:
+        # Check if the settings mean we want to reset the volume on startup
+        startupVol = Settings.getStartupVolume()
+
+        if startupVol < 0:
+            log("VideoScreensaverService: No Volume Change Required")
+        else:
+            log("VideoScreensaverService: Setting volume to %s" % startupVol)
+            xbmc.executebuiltin('SetVolume(%d)' % startupVol, True)
+
+        # Make sure that the settings have been updated correctly
+        Settings.cleanAddonSettings()
+
+        # Check if we should start the screensaver video on startup
+        if Settings.isLaunchOnStartup():
+            log("VideoScreensaverService: Launching screensaver video on startup")
+            xbmc.executebuiltin('RunScript(%s)' % (os.path.join(CWD, "screensaver.py")))
